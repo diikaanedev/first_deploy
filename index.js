1e10fcf8f888cef5a955306bbc51815ed34e9093
diikaanedev <diikaanedev@gmail.com>
2020-05-17 06:05:40 +0200

    pap est la

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = express();
 
 app.use('/', (req, res, next) => {
     let i = 0 ;
+    let i=2;
     res.send('ici on est en guerre ');
 });
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
-});
+}); 
